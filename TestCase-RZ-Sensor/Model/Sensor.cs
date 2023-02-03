ebb7d28ad0442e29004433075e7686647353a712
Sakuya LC <vadim12134@mail.ru>
2023-02-03 19:46:46 +0400

    Модель прибора

---
+++ TestCase-RZ-Sensor/Model/Sensor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace TestCase_RZ_Sensor.Model
+{
+    class Sensor
+    {
+        private byte state;
+        private bool fireAlarm;
+        private bool relayIsOn;
+        private bool relayIsOff;
+        private bool test;
+        private ulong serialNumber;
+
+        private Sensor(byte state, bool fireAlarm, bool relayIsOn, bool relayIsOff, bool test, ulong serialNumber)
+        {
+            this.state = state;
+            this.fireAlarm = fireAlarm;
+            this.relayIsOn = relayIsOn;
+            this.relayIsOff = relayIsOff;
+            this.test = test;
+            this.serialNumber = serialNumber;
+        }
+
+    }
+}
