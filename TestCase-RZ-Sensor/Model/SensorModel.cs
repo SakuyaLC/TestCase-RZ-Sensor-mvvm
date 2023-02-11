315412bf4a730c17664b1becbef1ea0baf9fa0bd
Sakuya LC <vadim12134@mail.ru>
2023-02-11 12:08:56 +0400

    Логика

--- TestCase-RZ-Sensor/Model/SensorModel.cs
+++ TestCase-RZ-Sensor/Model/SensorModel.cs
@@ -1,19 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace TestCase_RZ_Sensor.Model
 {
-    public class SensorModel : INotifyPropertyChanged
+    public class SensorModel : INotifyPropertyChanged, ISensor
     {
-        public byte State { get; set; }
-        public bool FireAlarm { get; set; }
-        public bool RelayIsOn { get; set; }
-        public bool Test { get; set; }
-        public ulong SerialNumber { get; set; }
+        private byte _state { get; set; }
+        private bool _fireAlarm { get; set; }
+        private bool _RelayIsOn { get; set; }
+        private bool _test { get; set; }
+        private ulong _serialNumber { get; set; }
+        private string _commands { get; set; }
+
+        //Парсинг ответа от прибора по его параметрам с формированием свойств по данным из пакета. Формат пакета (byte[])
+        private void ParseResponse()
+        {
+
+        }
+
+        //Формирование пакета команды в прибор – на выходе массив байт 
+        public byte[] FormCommandPacket(int command)
+        {
+            byte[] t1 = BitConverter.GetBytes(this.SerialNumber);
+            byte[] t2 = BitConverter.GetBytes(command);
+
+            byte[] commandPacket = commandPacket = t1.Concat(t2).ToArray();
+
+            return commandPacket;
+        }
+
+        public byte State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                OnPropertyChanged("State");
+            }
+        }
+
+        public bool FireAlarm
+        {
+            get { return _fireAlarm; }
+            set
+            {
+                _fireAlarm = value;
+                OnPropertyChanged("FireAlarm");
+            }
+        }
+
+        public bool RelayIsOn
+        {
+            get { return _RelayIsOn; }
+            set
+            {
+                _RelayIsOn = value;
+                OnPropertyChanged("RelayIsOn");
+            }
+        }
+
+        public bool Test
+        {
+            get { return _test; }
+            set
+            {
+                _test = value;
+                OnPropertyChanged("Test");
+            }
+        }
+
+        public ulong SerialNumber
+        {
+            get { return _serialNumber; }
+            set
+            {
+                _serialNumber = value;
+                OnPropertyChanged("SerialNumber");
+            }
+        }
+
+        public string Commands
+        {
+            get { return _commands; }
+            set
+            {
+                _commands = value;
+                OnPropertyChanged("Commands");
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
     }
 }
--- TestCase-RZ-Sensor/ViewModel/SensorViewModel.cs
+++ TestCase-RZ-Sensor/ViewModel/SensorViewModel.cs
@@ -1,31 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 using TestCase_RZ_Sensor.Model;
 
 namespace TestCase_RZ_Sensor.ViewModel
 {
     public class SensorViewModel
     {
+        public ObservableCollection<SensorModel> Sensors { get; private set; }
+
+        public int command;
+
+        public DelegateCommand CmEnter1 { get; private set; }
+        public DelegateCommand CmEnter2 { get; private set; }
+        public DelegateCommand CmEnter3 { get; private set; }
+        public DelegateCommand CmEnter4 { get; private set; }
+        public DelegateCommand CmEnter5 { get; private set; }
+        public DelegateCommand CmEnter6 { get; private set; }
+        public DelegateCommand CmSend { get; private set; }
+        public DelegateCommand CmClear { get; private set; }
 
         public SensorViewModel()
         {
-            List<SensorModel> sensorsList = new List<SensorModel>() {
-                new SensorModel() {State = 0, FireAlarm = false, RelayIsOn = true, Test = true, SerialNumber = 222345 } };
-            Sensors = new List<SensorModel>(sensorsList);
+            Sensors = new ObservableCollection<SensorModel>()
+            {
+                new SensorModel() {State = 0, FireAlarm = false, RelayIsOn = true, Test = true, SerialNumber = 222345, Commands = ""},
+            };
+
+            CmEnter1 = new DelegateCommand(CommandEnter1, CanEnterCommand);
+            CmEnter2 = new DelegateCommand(CommandEnter2, CanEnterCommand);
+            CmEnter3 = new DelegateCommand(CommandEnter3, CanEnterCommand);
+            CmEnter4 = new DelegateCommand(CommandEnter4, CanEnterCommand);
+            CmEnter5 = new DelegateCommand(CommandEnter5, CanEnterCommand);
+            CmEnter6 = new DelegateCommand(CommandEnter6, CanEnterCommand);
+            CmSend = new DelegateCommand(CommandSend, CanEnterCommand);
+            CmClear = new DelegateCommand(CommandClear, CanEnterCommand);
+        }
+
+        public void CommandEnter1(object message)
+        {
+            Sensors[0].Commands = "1";
+        }
+
+        public void CommandEnter2(object message)
+        {
+            Sensors[0].Commands = "2";
+        }
+
+        public void CommandEnter3(object message)
+        {
+            Sensors[0].Commands = "3";
+        }
+
+        public void CommandEnter4(object message)
+        {
+            Sensors[0].Commands = "4";
+        }
+
+        public void CommandEnter5(object message)
+        {
+            Sensors[0].Commands = "5";
+        }
+
+        public void CommandEnter6(object message)
+        {
+            Sensors[0].Commands = "6";
+        }
+
+        async public void CommandSend(object message)
+        {
+            await Task.Run(
+                () =>
+                {
+                    command = Int32.Parse(Sensors[0].Commands);
+
+                    switch (command)
+                    {
+                        case 1:
+                            Sensors[0].State = 0;
+                            break;
+                        case 2:
+                            Sensors[0].State = 2;
+                            break;
+                        case 3:
+                            Sensors[0].Test = !Sensors[0].Test;
+                            break;
+                        case 4:
+                            Sensors[0].State = 1;
+                            break;
+                        case 5:
+                            Sensors[0].RelayIsOn = true;
+                            break;
+                        case 6:
+                            Sensors[0].RelayIsOn = false;
+                            break;
+                    }
+
+                    Trace.WriteLine(Sensors[0].FormCommandPacket(command));
+
+                    Sensors[0].Commands = "";
+                }
+            );
 
         }
 
-        private List<SensorModel> _Sensors;
-
-        public List<SensorModel> Sensors
+        public void CommandClear(object message)
         {
-            get { return _Sensors; }
-            set { _Sensors = value; }
+            Sensors[0].Commands = "";
+        }
+
+        public bool CanEnterCommand(object message)
+        {
+            return true;
         }
     }
 }
